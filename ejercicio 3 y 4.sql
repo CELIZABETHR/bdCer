a541c2962c846dc4f186f3508deba09181bda3d2
CELIZABETHR <eli_rodriz_05@hotmail.com>
2017-05-15 21:43:41 -0300

    Update ejercicio 3 y 4.sql

--- ejercicio 3 y 4.sql
+++ ejercicio 3 y 4.sql
@@ -9,12 +9,12 @@ inciso 3
 Consultas
 1.select count (*) from tarea where estado=pendiente
 2.select count (*) from tarea where estado=finalizada
-3.select comentario.detalle from usuario a inner join on usuario.id=comentario.id where usuario.nombre='u'
-4.select count checklist from tarea a inner join on tarea.id=checklist.id where tarea.nombre='t'
-5.select order asc comentarios.detalle from usuario a inner join on usuario.id=comentarios.id where usuario.nombre='u'
-6.select order desc comentario.detalle from usuario a inner join on usuario.id=comentario.id where usuario.nombre='u'
-7.select count comentario.detalle from usuario a inner join on usuario.id=comentario.id where usuario.nombre='z'
-8.select count tarea from usuario a inner join on usuario.id=tarea.id where usuario.nombre='z'
+3.select c.detalle from comentario as c inner join usuarios as u on (c.usuario_id < usuario_id) where u.nombre where u nombre='u' 
+4.select count c.checklist from tarea as c inner join tarea as u on(tarea.id=checklist.id) where tarea.nombre='t'
+5.select c.detalle from comentario as c inner join usuarios as u on (c.usuario_id < usuario_id) where u.nombre where u nombre='u' orden by c.detalle
+6.select c.detalle from comentario as c inner join usuarios as u on (c.usuario_id < usuario_id) where u.nombre where u nombre='u' orden by desc c.detalle
+7.select c.detalle from comentario as c inner join usuarios as z on (c.usuario_id < usuario_id) where z.nombre where z nombre='z'
+8.select count(*) t.detalle from tarea as t inner join usuarios as z on (t.usuario_id < usuario_id) where z.nombre where z nombre='z' 
 9.select min fecha from comentario
-10.select min fecha from comentarios a inner join on comentario.id=tarea.id where tarea.nombre='t'
-11.select count tarea from usuario a inner join on usuario.id=tarea.id where usuario.nombre='u'
+10.select min (fecha) from comentarios a inner join tarea on comentario.id=tarea.id where tarea.nombre='t'
+11.select count(*)from usuario a inner join tarea on usuario.id=tarea.id where usuario.nombre='u'
