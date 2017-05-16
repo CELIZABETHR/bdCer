b9f16bcc1b13066055f5763b591591fc0e6d0854
CELIZABETHR <eli_rodriz_05@hotmail.com>
2017-05-15 21:50:53 -0300

    Update ejercicio 3 y 4.sql

--- ejercicio 3 y 4.sql
+++ ejercicio 3 y 4.sql
@@ -15,6 +15,6 @@ Consultas
 6.select c.detalle from comentario as c inner join usuarios as u on (c.usuario_id < usuario_id) where u.nombre where u nombre='u' orden by desc c.detalle
 7.select c.detalle from comentario as c inner join usuarios as z on (c.usuario_id < usuario_id) where z.nombre where z nombre='z'
 8.select count(*) t.detalle from tarea as t inner join usuarios as z on (t.usuario_id < usuario_id) where z.nombre where z nombre='z' 
-9.select min fecha from comentario
-10.select min (fecha) from comentarios a inner join tarea on comentario.id=tarea.id where tarea.nombre='t'
+9.select fecha_realizado from comentario limit 1
+10.select fecha_realizado from comentarios a inner join tarea on comentario.id=tarea.id where tarea.nombre='t'
 11.select count(*)from usuario a inner join tarea on usuario.id=tarea.id where usuario.nombre='u'
