b6875c5debdb5d930c3eae8ccc454b5127052a2d
Elizabeth <eli_rodriz_05@hotmail.com>
2017-06-06 14:09:30 -0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -3,12 +3,18 @@ INSERT INTO `usuario` (`usuario_id`, `nombre`, `apellido`) VALUES (NULL, 'sol', 
 INSERT INTO `usuario` (`usuario_id`, `nombre`, `apellido`) VALUES (NULL, 'olvaldo', 'Garcia');
 INSERT INTO `usuario` (`usuario_id`, `nombre`, `apellido`) VALUES (NULL, 'Juan', 'Martinez');
 
-INSERT INTO `tarea` (`tarea_id`, `nombre`, `inicio`, `estimacion`, `consumo`, `fin`, `estado`) VALUES (NULL, 'hago el desayuno', '2017-05-15 06:15:00', '1', '1', '2017-05-15 08:00:00', 'Realizado');
-UPDATE `tarea` SET `inicio` = '2017-05-15 06:10:00', `estimacion` = '0.20', `consumo` = '0.15', `fin` = '2017-05-15 06:25:00' WHERE `tarea`.`tarea_id` = 1;
 
-INSERT INTO `tarea` (`tarea_id`, `nombre`, `inicio`, `estimacion`, `consumo`, `fin`, `estado`) VALUES (NULL, 'Ir al trabajo', '2017-05-15 06:30:00', '0.15', '0.10', '2017-05-15 06:40:00', 'Llegue');
-INSERT INTO `tarea` (`tarea_id`, `nombre`, `inicio`, `estimacion`, `consumo`, `fin`, `estado`) VALUES (NULL, 'hacer almuerzo', '2017-05-15 14:05:00', '1', '1', '2017-05-15 15:00:00', 'Realizado');
 
+INSERT INTO `tarea` (`Tarea_id`, `nombre`, `inicio`, `estimacion`, `consumo`, `fin`, `estado`) VALUES
+(1, 'hacer la cama', '2017-05-15', 0, 0, '2017-05-15 06:25:00', 'Realizado'),
+(2, 'Ir al gym', '2017-05-15', 0, 0, '2017-05-15 06:40:00', 'Llegue'),
+(3, 'Cocinar', '2017-05-15', 1, 1, '2017-05-15 15:00:00', 'Realizado'),
+(4, 'Dormir la siesta', '2017-05-15', 1, 1, '2017-05-15 01:00:00', 'Realizado'),
+(5, 'Ducharme', '2017-05-15', 0, 0, '2017-05-15 06:40:00', 'Llegue'),
+(6, 'Ir a cursar', '2017-05-15', 1, 1, '2017-05-15 15:00:00', 'Realizado'),
+(7, 'Cenar', '2017-05-15', 1, 1, '2017-05-15 01:00:00', 'Realizado'),
+(8, 'Mirar television', '2017-05-15', 0, 0, '2017-05-15 06:40:00', 'Llegue'),
+(9, 'Preparar almuerzo', '2017-05-15', 1, 1, '2017-05-15 15:00:00', 'Realizado');
 
 INSERT INTO `pasos`(`pasos_id`, `nombre`, `estado`, `tarea_id`) VALUES (null,'hacer la merienda','Listo',1);
 INSERT INTO `pasos`(`pasos_id`, `nombre`, `estado`, `tarea_id`) VALUES (null,'limpiar la casa','Listo',1);
